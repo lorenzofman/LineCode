60b1c5fcdbea765668b52d1119e061f1e8dbc21e
menegais1 <menegais1@gmail.com>
2020-07-05 21:49:32 -0300

    Fixed some wrong codes, still need more testing, the grid gets confusing when using manchester and differential manchester

--- LineCodes.py
+++ LineCodes.py
@@ -4,32 +4,37 @@ import gc
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib.ticker as ticker
 
-high_level = 13
+high_level = 5
 mid_level = 0
-low_level = -13
+low_level = -5
 
 
+# O protocolo RS-232 define uma faixa de tensões para considerar os sinais, sendo elas:
+# – -3 V a -15V como Marca = 1 = OFF
+# – +3V a +15V como Espaço = 0 = ON (Pronto)
+# Optamos por considerar um valor médio entre o máximo e minimo definidos, sendo então -9 e 9 volts
 def nrz_l(bits):
     encoded = []
     for bit in bits:
         if bit == 1:
-            encoded.append(high_level)
-        if bit == 0:
             encoded.append(low_level)
+        if bit == 0:
+            encoded.append(high_level)
     return encoded
 
 
 # A string of zeros causes the NRZI data toggle each bit time, while a string
 # of ones causes long period with no transition in the data. (USB NRZI)
 # http://www.ece.ualberta.ca/~elliott/ee552/studentAppNotes/2001f/interfacing/usb/appl_note.html
+# The USB protocol use bit-stuffing after every sequence of six '1', but we don't implement this here
 def nrz_i(bits):
     encoded = []
     # Check if USB starts in low/high
-    low = False
+    low = True
     for bit in bits:
-        encoded.append(low_level if low else high_level)
-        if bit == 0:
+        if bit == 1:
             low = not low
+        encoded.append(high_level if low else low_level)
     return encoded
 
 
@@ -57,24 +62,26 @@ def manchester(bits):
     encoded = []
     for bit in bits:
         if bit == 0:
-            encoded.append(low_level)
             encoded.append(high_level)
+            encoded.append(low_level)
         else:
-            encoded.append(high_level)
             encoded.append(low_level)
+            encoded.append(high_level)
     return encoded
 
 
 def differential_manchester(bits):
-    # Todo: Study about how first bit is encoded - for now, it's encoded with manchester
-    encoded = manchester([bits[0]])
-    first = encoded[0]
-    second = encoded[1]
-    for bit in bits[1:]:
-        if bit == 1:
+    encoded = []
+    first = low_level
+    second = high_level
+    for bit in bits:
+        if bit == 0:
+            encoded.append(first)
+            encoded.append(second)
+        else:
             first, second = second, first
-        encoded.append(first)
-        encoded.append(second)
+            encoded.append(first)
+            encoded.append(second)
     return encoded
 
 
@@ -84,11 +91,10 @@ from enum import Enum
 class LineCode(Enum):
     nrz_l = 1
     nrz_i = 2
-    bipolar = 3
-    ami = 4
-    pseudo_ternary = 5
-    manchester = 6
-    differential_manchester = 7
+    ami = 3
+    pseudo_ternary = 4
+    manchester = 5
+    differential_manchester = 6
 
 
 # It's using monstrous 21 GB
@@ -96,7 +102,6 @@ gc.enable()
 
 # Todo: Fetch data from input
 
-# binaryString = input("ENter binary sequence: ")
 print("The sequence should be input in the format: '1 0 1 0 1 0 0', without quotes and every bit separated by a space")
 binary = list(map(int, input("\nEnter binary sequence: ").strip().split()))
 
@@ -104,15 +109,14 @@ binary = list(map(int, input("\nEnter binary sequence: ").strip().split()))
 assert all(bit == 0 or bit == 1 for bit in binary)
 
 lineCoding = int(input(
-    "Select line coding: \n1 - nrz_l;\n2 - nrz_i;\n3 - bipolar;\n4 - ami;\n5 - pseudo_ternary;\n6 - manchester;\n7 - differential manchester;\nLine code:"))
+    "Select line coding: \n1 - nrz_l;\n2 - nrz_i;\n3 - ami;\n4 - pseudo_ternary;\n5 - manchester;\n6 - differential "
+    "manchester;\nLine code:"))
 
 y = []
 if LineCode(lineCoding) == LineCode.nrz_l:
     y = nrz_l(binary)
 elif LineCode(lineCoding) == LineCode.nrz_i:
     y = nrz_i(binary)
-elif LineCode(lineCoding) == LineCode.bipolar:
-    y = bipolar(binary,0)
 elif LineCode(lineCoding) == LineCode.ami:
     y = ami(binary)
 elif LineCode(lineCoding) == LineCode.pseudo_ternary:
